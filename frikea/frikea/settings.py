67c920e1d19020fe1dc162a91af242ac4dba541a
santiagofp9 <santiagofp9@gmail.com>
2020-02-25 17:07:42 +0100

    BDD
    La base funciona y hay unos poquitos datos creados desde admin

--- frikea/frikea/settings.py
+++ frikea/frikea/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tienda',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,8 @@ WSGI_APPLICATION = 'frikea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'frikea',
     }
 }
 
--- frikea/tienda/models.py
+++ frikea/tienda/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=300)
+    precio = models.FloatField(default=10)
+    stock = models.IntegerField()
+    imagen = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.nombre
+
+class Carrito(models.Model):
+    producto = models.CharField(max_length=30)
+    usuario = models.CharField(max_length=30)
+    cantidad = models.IntegerField()
+
+class Usuario(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.username
+
+
