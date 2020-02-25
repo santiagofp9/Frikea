5a135fb67628e2604542452ae2f52a52db932184
santiagofp9 <santiagofp9@gmail.com>
2020-02-25 17:54:46 +0100

    casi se ve bien
    seguimos las instrucciones pero no funciona el template en django

--- frikea/tienda/views.py
+++ frikea/tienda/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import ListView, TemplateView, CreateView
+from django.urls import reverse_lazy
+"""from .forms import EstuForm"""
+from .models import *
 
 # Create your views here.
+
+class Inicio(TemplateView):
+    template_name = 'tienda/index.html'
