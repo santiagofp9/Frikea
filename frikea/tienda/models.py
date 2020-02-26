eef985b263f1b1ec35c7de4b4d978ff72adb45be
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-02-26 17:57:23 +0100

    2

--- frikea/tienda/models.py
+++ frikea/tienda/models.py
@@ -19,7 +19,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=30)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
     descripcion = models.TextField(max_length=300)
-    precio = models.DecimalField(default=10)
+    precio = models.DecimalField(decimal_places=2, max_digits=6)
     stock = models.IntegerField()
     imagen = models.ImageField()
 
