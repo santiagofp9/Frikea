d81e171a87fea6effb617985f18da53cf8e8d691
santiagofp9 <santiagofp9@gmail.com>
2020-02-27 16:29:30 +0100

    imágenes
    las fotos ya van alegres a su carpeta iemegés

--- frikea/tienda/models.py
+++ frikea/tienda/models.py
@@ -21,7 +21,7 @@ class Producto(models.Model):
     descripcion = models.TextField(max_length=300)
     precio = models.DecimalField(decimal_places=2, max_digits=6)
     stock = models.IntegerField()
-    imagen = models.ImageField()
+    imagen = models.ImageField(upload_to = 'iemeges/')
 
     def __str__(self):
         return self.nombre
