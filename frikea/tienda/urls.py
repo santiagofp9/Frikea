f17c17456477bb13dda79b1c61db04fccb20f7e5
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-03-02 16:48:32 +0100

    lali

--- frikea/tienda/urls.py
+++ frikea/tienda/urls.py
@@ -6,6 +6,8 @@ from .views import *
 urlpatterns = [
     path('', Inicio.as_view(), name='inicio'),
     path('productos/', ListaProducto.as_view(), name='productos'),
+    path('listcategorias/', ListaCategorias.as_view(), name='listcategorias'),
+    path('producategorias/<int:pk>', CategoriaProducto.as_view(), name='producategorias'),
     path('about/', About.as_view(), name='about'),
     
 
--- frikea/tienda/views.py
+++ frikea/tienda/views.py
@@ -5,6 +5,8 @@ from .models import *
 
 # Create your views here.
 
+
+    
 class Inicio(ListView):
     model = Producto
     template_name = 'tienda/index.html'
@@ -19,3 +21,21 @@ class ListaProducto(ListView):
     template_name = 'tienda/product.html'
     context_object_name = 'produ'
     queryset = Producto.objects.all()
+
+
+class ListaCategorias(ListView):
+    model = Categoria
+    template_name = 'tienda/categories.html'
+    context_object_name = 'catego'
+    queryset = Categoria.objects.all()
+
+
+class CategoriaProducto(ListView):
+    model = Producto
+    template_name = 'tienda/product.html'
+
+    def get_context_data(self, **kwargs):
+       	context = super(CategoriaProducto, self).get_context_data(**kwargs)
+       	parametro = self.kwargs.get('pk', None)
+       	context['produ'] = Producto.objects.filter(categoria=parametro)
+       	return context
